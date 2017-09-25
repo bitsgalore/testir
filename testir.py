deb7af71c6b77d080add9e3f3d687f7f62aff181
bitsgalore <johan.vanderknijff@kb.nl>
2017-09-25 18:04:41 +0200

    DIDL parsing now work

--- testir.py
+++ testir.py
@@ -3,6 +3,12 @@
 from lxml import etree
 
 def parseDidl(didl):
+    """Parse DIDL and return list of all resources"""
+
+    # List for storing extracted resources
+    resources = []
+
+    # Namespace definitions
     oai_pmh_ns = 'http://www.openarchives.org/OAI/2.0/'
     didl_ns = 'urn:mpeg:mpeg21:2002:02-DIDL-NS'
     didl_combined_ns = 'http://gh.kb-dans.nl/combined/v0.9/'
@@ -12,25 +18,33 @@ def parseDidl(didl):
               "xsi": xsi_ns,
               "didl": didl_ns,
               "dc": didl_combined_ns}
-    
+
+    # Parse DIDL
     parser = etree.XMLParser()          
     root = etree.parse(didl, parser)
-    DIDL = root.find('//ns:GetRecord/ns:record/ns:metadata/dc:nl_didl_combined/dc:nl_didl_norm/didl:DIDL/', 
+
+    # We're only interested in nl_didl_norm and its child elements  
+    DIDL = root.find('//ns:GetRecord/ns:record/ns:metadata/dc:nl_didl_combined/dc:nl_didl_norm/didl:DIDL', 
                              namespaces=NSMAP)
 
-    #nl_didl_norm = root.find('OAI-PMH', namespaces=NSMAP)
-    # nl_didl_norm = tree.find("OAI-PMH/nl_didl_norm")
-    print(DIDL)
+    # Get list of all didl:Resource elements
+    resourceElts = DIDL.xpath('//didl:Item/didl:Component/didl:Resource', namespaces=NSMAP)
 
-    #for identifierURI in identifiersURI:
-    #    modsIdentifierURI = etree.SubElement(modsRelatedItem, "{%s}identifier" %(config.mods_ns))
-    #    modsIdentifierURI.attrib["type"] = "uri"
-    #    modsIdentifierURI.text = identifierURI
+    # Iterate over Resource elements and get URLs from 'ref' attribute values 
+    for resourceElt in resourceElts:
+        try:
+            url = resourceElt.attrib['ref']
+            resources.append(url)
+        except KeyError:
+            pass
+
+    return resources
 
 
 def main():
     didl = "./didl.xml"
-    parseDidl(didl)
+    resources = parseDidl(didl)
+    print(resources)
 
 if __name__ == "__main__":
     main()
